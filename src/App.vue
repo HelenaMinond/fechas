f1459d5a554ca76ac56c07a11f848183e8cc4ed6
Helena Santana <helena.minond@gmail.com>
2021-04-03 18:05:33 -0300

    proyecto 'fechas'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <h1 class="my-5">Cuenta regresiva</h1>
+
+    <h2>{{ tiempoInicial }} seg</h2>
+
+    <button
+      class="mx-3 mt-4 px-2"
+      v-for="boton in arrBotones"
+      :key="boton.name"
+      @click="start(boton.valor)"
+    >
+      {{ boton.name }}
+    </button>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      arrBotones: [
+        { name: "3s", valor: 3 },
+        { name: "1m", valor: 60 },
+        { name: "5m", valor: 300 },
+        { name: "10m", valor: 600 },
+        { name: "30m", valor: 1800 },
+      ],
+      tiempoInicial: "0000",
+      tiempoFinal: "",
+      tiempo: "",
+    };
+  },
+  methods: {
+    start(val) {
+      this.tiempoInicial = val;
+      this.tiempoFinal = this.tiempoInicial - val;
+
+      this.tiempo = setInterval(() => {
+        this.tiempoInicial--;
+        if (this.tiempoInicial == this.tiempoFinal) {
+          clearInterval(this.tiempo);
+        }
+      }, 1000);
+    },
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+div {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+h1,
+h2 {
+  font-weight: 700;
+}
+
+h2 {
+  color: rgb(170, 169, 169);
+}
+
+button {
+  border: 0px;
+  background: rgb(206, 206, 206);
+}
+
+button:hover {
+  border: solid 3px rgb(91, 186, 218);
+  border-radius: 5px;
 }
 </style>
